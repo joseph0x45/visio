540585129a8ee43ccdd5f057724d361875ea2191
TheWisePigeon <zozozozeph@gmail.com>
2023-11-03 19:21:31 +0000

    update database schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,8 +1,15 @@
-create table users(
+create table users (
   id uuid not null primary key,
   github_id text not null unique,
   username text not null unique,
   email text not null unique,
   avatar text not null,
   plan text default 'basic'
+);
+
+create table keys (
+  id uuid not null primary key,
+  prefix text not null unique,
+  owner uuid not null references users(id),
+  key_hash text not null
 );
