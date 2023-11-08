d6118d2e5d479b6e0bc8495594a90d053273e2d1
TheWisePigeon <zozozozeph@gmail.com>
2023-11-08 19:53:26 +0000

    show progress while navigating

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,24 @@
-<script>
-  import "../app.css";
-  import { Toaster } from "svelte-french-toast"
+<script lang="ts">
+	import '../app.css';
+	import { Toaster } from 'svelte-french-toast';
+	import { navigating } from '$app/stores';
+	import { browser } from '$app/environment';
+	import NProgress from 'nprogress';
+	import 'nprogress/nprogress.css';
+	NProgress.configure({
+		minimum: 0.16
+	});
+	$: {
+		if (browser) {
+			if ($navigating) {
+				NProgress.start();
+			}
+			if (!$navigating) {
+				NProgress.done();
+			}
+		}
+	}
 </script>
 
-<Toaster/>
+<Toaster />
 <slot />
