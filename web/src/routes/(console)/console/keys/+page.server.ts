f4c53e453daacfa6c14868386c4e306ad3c9a5d1
TheWisePigeon <zozozozeph@gmail.com>
2023-11-10 11:41:38 +0000

    delete keys on console

--- web/src/routes/(console)/console/keys/+page.server.ts
+++ web/src/routes/(console)/console/keys/+page.server.ts
@@ -22,7 +22,6 @@ export const load: PageServerLoad = async ({ cookies }) => {
     }
   )
   const data = await response.json() as Array<Key>
-  console.log(data)
   return {
     keys: data
   }
@@ -53,6 +52,35 @@ export const actions: Actions = {
       return fail(response.status)
     } catch (error) {
       console.log(`Error while creating key: ${error}`)
+      return fail(500)
+    }
+  },
+  delete: async ({ cookies, request }) => {
+    try {
+      const auth_token = cookies.get("auth_token")
+      if (!auth_token) {
+        throw redirect(301, "/")
+      }
+      const data = await request.formData()
+      const key_prefix = data.get('prefix')
+      if (!key_prefix) {
+        return fail(500)
+      }
+      const response = await fetch(
+        `http://localhost:8080/keys/${key_prefix}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Authorization": `Bearer ${auth_token}`
+          }
+        }
+      )
+      if (response.status == 200) {
+        return {}
+      }
+      return fail(response.status)
+    } catch (error) {
+      console.log(`Error while deleting key ${error}`)
       return fail(500)
     }
   }
