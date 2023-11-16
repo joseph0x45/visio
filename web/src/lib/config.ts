ef3b42eceb19ab65b8f2bd9f3a623698e5ab223d
TheWisePigeon <zozozozeph@gmail.com>
2023-11-16 21:34:33 +0000

    add api prod url

--- web/src/lib/config.ts
+++ web/src/lib/config.ts
@@ -1,3 +1,2 @@
-// export const API_URL = "https://api.getvisio.cloud"
+export const API_URL = "https://api.getvisio.cloud"
 // export const API_URL = "http://localhost:8080"
-export const API_URL = "https://2140-102-64-173-21.ngrok-free.app"
