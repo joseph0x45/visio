af1ae589d433fc8888aa30a69c3082f41ccdbaaf
TheWisePigeon <zozozozeph@gmail.com>
2023-11-16 16:52:45 +0000

    add dev server url

--- web/src/lib/config.ts
+++ web/src/lib/config.ts
@@ -1,2 +1,3 @@
-export const API_URL = "https://api.getvisio.cloud"
+// export const API_URL = "https://api.getvisio.cloud"
 // export const API_URL = "http://localhost:8080"
+export const API_URL = "https://2140-102-64-173-21.ngrok-free.app"
