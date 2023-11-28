d26658a995232f5f344764e196fcc1795990eaa5
TheWisePigeon <zozozozeph@gmail.com>
2023-11-28 10:44:37 +0000

    bye bye old visio

--- server/models/face.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Face struct {
-	Id          string `json:"id" db:"id"`
-	CreatedBy   string `json:"created_by" db:"created_by"`
-	Descriptor  string `json:"descriptor" db:"descriptor"`
-	CreatedAt   string `json:"created_at" db:"created_at"`
-	LastUpdated string `json:"last_updated" db:"last_updated"`
-}
