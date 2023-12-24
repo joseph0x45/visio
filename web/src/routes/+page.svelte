8cb756b2be8bdc552181bac642d26de7ce6c4bd4
TheWisePigeon <zozozozeph@gmail.com>
2023-12-24 23:09:41 +0000

    ui updates and added redis service

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,16 +1,4 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-	async function github_auth() {
-		try {
-			let response = await fetch(`http://localhost:8080/auth/request`);
-			const { url } = (await response.json()) as { url: string };
-			goto(url);
-		} catch (err) {
-			console.log(err);
-		}
-	}
-</script>
-
-<button on:click={github_auth}>
-  <h1>Login with Github</h1>
-</button>
+<h1>Welcome to Visio</h1>
+<a href="/auth">
+	<h1>Go to auth page</h1>
+</a>
