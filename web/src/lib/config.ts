757615d81b19269ac80f349ec7db33bbd8ad4863
TheWisePigeon <zozozozeph@gmail.com>
2023-12-29 10:03:50 +0000

    add dev server url

--- web/src/lib/config.ts
+++ web/src/lib/config.ts
@@ -1,3 +1,3 @@
 // export const API_URL = "https://api.getvisio.cloud"
-export const API_URL = "http://localhost:8080"
-// export const API_URL = "https://visio-dev.onrender.com"
+// export const API_URL = "http://localhost:8080"
+export const API_URL = "https://visio-dev.onrender.com"
