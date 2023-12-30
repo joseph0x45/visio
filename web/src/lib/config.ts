d175f19a3f0893fcf8e222fdf61db6dde9e2f652
TheWisePigeon <zozozozeph@gmail.com>
2023-12-30 08:24:51 +0000

    remove sveltekit app

--- web/src/lib/config.ts
+++
@@ -1,3 +0,0 @@
-// export const API_URL = "https://api.getvisio.cloud"
-// export const API_URL = "http://localhost:8080"
-export const API_URL = "https://visio-dev.onrender.com"
--- web/src/lib/stores.ts
+++
@@ -1,3 +0,0 @@
-import { writable } from "svelte/store"
-
-export const location = writable('home')
