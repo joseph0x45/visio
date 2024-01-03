0ce8b14e065af7ba99af665fe0cc81062cd068cc
TheWisePigeon <zozozozeph@gmail.com>
2024-01-03 13:52:35 +0000

    feat: add cookie auth based middleware and minor updates to home page

---
+++ internal/middlewares/auth.go
@@ -0,0 +1,52 @@
+package middlewares
+
+import (
+	"errors"
+	"github.com/gofiber/fiber/v2"
+	"log/slog"
+	"visio/internal/store"
+	"visio/internal/types"
+)
+
+type AuthMiddleware struct {
+	sessions *store.Sessions
+	users    *store.Users
+	logger   *slog.Logger
+}
+
+func NewAuthMiddleware(sessions *store.Sessions, users *store.Users, logger *slog.Logger) *AuthMiddleware {
+	return &AuthMiddleware{
+		sessions: sessions,
+		users:    users,
+		logger:   logger,
+	}
+}
+
+func (m *AuthMiddleware) CookieAuth(c *fiber.Ctx) error {
+	sessionId := c.Cookies("session")
+	if sessionId == "" {
+		return c.Redirect("/auth", fiber.StatusFound)
+	}
+	sessionValue, err := m.sessions.Get(sessionId)
+	if err != nil {
+		if errors.Is(err, types.ErrSessionNotFound) {
+			return c.Redirect("/auth", fiber.StatusFound)
+		}
+		m.logger.Error(err.Error())
+		return c.Redirect("/auth", fiber.StatusFound)
+	}
+	sessionUser, err := m.users.GetById(sessionValue)
+	if err != nil {
+		if errors.Is(err, types.ErrUserNotFound) {
+			return c.Redirect("/auth", fiber.StatusFound)
+		}
+		m.logger.Error(err.Error())
+		return c.Redirect("/auth", fiber.StatusFound)
+	}
+	c.Locals("currentUser", sessionUser)
+	return c.Next()
+}
+
+func (m *AuthMiddleware) KeyAuth(c *fiber.Ctx) error {
+	return c.Next()
+}
